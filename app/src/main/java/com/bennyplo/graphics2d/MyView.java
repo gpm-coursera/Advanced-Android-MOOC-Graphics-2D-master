aa161b2dae8d2240bbb5ace81f3a2dd40f0c3058
Gopi Palaniappan <gopi.pm@gmail.com>
2022-12-04 02:34:14 -0800

    Poly Line

--- app/src/main/java/com/bennyplo/graphics2d/MyView.java
+++ app/src/main/java/com/bennyplo/graphics2d/MyView.java
@@ -3,6 +3,7 @@ package com.bennyplo.graphics2d;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.view.View;
 
 /**
@@ -12,6 +13,8 @@ import android.view.View;
 public class MyView extends View {
     private Paint redPaint;
     private Paint bluePaint;
+    private Paint greenPaint;
+    private Path mylines;
 
 
     public MyView(Context context) {
@@ -26,6 +29,46 @@ public class MyView extends View {
         bluePaint.setStyle(Paint.Style.STROKE);//stroke only no fill
         bluePaint.setColor(0xFF0000FF);//blue color
         bluePaint.setStrokeWidth(5);//set the line stroke width to 5
+
+        greenPaint = new Paint();
+        greenPaint.setARGB(255,0,255,0);
+
+        mylines =new Path();
+        /*
+        mylines.moveTo(100,100);
+        mylines.lineTo(150,200);
+        mylines.lineTo(160,200);
+
+        mylines.moveTo(160,200);
+        mylines.lineTo(200,150);
+        mylines.lineTo(210,150);
+
+        mylines.moveTo(210,150);
+        mylines.lineTo(250,250);
+        mylines.lineTo(260,250);
+
+        mylines.moveTo(260,250);
+        mylines.lineTo(300,150);
+        mylines.lineTo(310,150);
+       */
+      //  mylines.lineTo(250,250);
+      //  mylines.lineTo(300,200);
+       // mylines.lineTo(250,250);
+       // mylines.lineTo(200,150);
+       // mylines.lineTo(150,200);
+       // mylines.moveTo(100,100);
+
+        mylines.moveTo(50,300);
+        mylines.lineTo(160,280);
+        mylines.lineTo(300,380);
+        mylines.lineTo(380,370);
+        mylines.lineTo(280,450);
+        mylines.lineTo(100,390);
+        mylines.lineTo(160,380);
+        mylines.lineTo(50,300);
+
+
+
     }
 
 
@@ -37,5 +80,7 @@ public class MyView extends View {
 
         canvas.drawCircle(600,600,145,redPaint);
         //canvas.drawCircle(500,450,50,redPaint);
+
+        canvas.drawPath(mylines, greenPaint);
     }
 }
